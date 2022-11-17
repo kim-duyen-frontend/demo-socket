cebb949f852a382d021605616f500597de1350cb
kim-duyen-frontend <ntduyen255@gmail.com>
2022-11-17 11:51:53 +0700

    add jwt for register

--- socket/src/services/auth.js
+++ socket/src/services/auth.js
@@ -1,5 +1,6 @@
 import db from "../models";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const hashPassword = (password) => {
     let salt = bcrypt.genSaltSync(10);
@@ -17,10 +18,15 @@ export const register = ({ username, email, password }) => new Promise(async (re
                 password: hashPassword(password)
             }
         })
-        console.log(response);
+        // console.log(response[0].id);
+        // console.log(response[0].email);
+        // console.log(response[0].roleID);
+        //Create token
+        const token = response[1] ? jwt.sign({ id: response[0].id, email: response[0].email, roleID: response[0].roleID }, process.env.TOKEN_SECRET, { expiresIn: "5d" }) : null;
         resolve({
             err: response[1] ? 0 : 1,
-            message: response[1] ? "Register successfully" : "Email has already existed"
+            message: response[1] ? "Register successfully" : "Email has already existed",
+            token
         });
     } catch (error) {
         reject(error)
