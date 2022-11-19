064ae94e9db0847d2561dcf1027cf11c1043a231
kim-duyen-frontend <ntduyen255@gmail.com>
2022-11-19 23:38:27 +0700

    private page home

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,13 +1,20 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { getUserLogin } from '../../redux/apiRequest';
 
 function Home() {
     const user = useSelector((state) => state.auth.login?.currentUser);
     const dispatch = useDispatch();
+    const navigate = useNavigate();
 
     useEffect(() => {
-        getUserLogin(user?.access_token, dispatch);
+        if (!user) {
+            navigate("/");
+        } else {
+            getUserLogin(user?.access_token, dispatch);
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
     return (
         <div>
